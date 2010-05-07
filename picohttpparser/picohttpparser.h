eccf73d9b1352dafc046f9bafdd35659517f6095
Kazuho Oku <kazuhooku@gmail.com>
2010-05-07 17:05:04 +0900

    response parser will be provided as a separate module (Revert "update picohttpparser to 5446aad66bdb9261d888")
    This reverts commit 44d466e28395392da53d5dcf9068739f6003de33.

--- picohttpparser/picohttpparser.h
+++ picohttpparser/picohttpparser.h
@@ -1,10 +1,6 @@
 #ifndef picohttpparser_h
 #define picohttpparser_h
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /* contains name and value of a header (name == NULL if is a continuing line
  * of a multiline header */
 struct phr_header {
@@ -21,15 +17,5 @@ int phr_parse_request(const char* buf, size_t len, const char** method,
 		      size_t* path_len, int* minor_version,
 		      struct phr_header* headers, size_t* num_headers,
 		      size_t last_len);
-
-/* ditto */
-int phr_parse_response(const char* _buf, size_t len, int *minor_version,
-              int *status, const char **msg, size_t *msg_len,
-              struct phr_header* headers, size_t* num_headers,
-              size_t last_len);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
