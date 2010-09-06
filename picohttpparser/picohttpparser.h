6c8b280605901fd89c0648eaf4d9af50200c53ab
gfx <gfuji@cpan.org>
2010-09-06 18:31:41 +0900

    Update picohttpparser.c to eccf73d9b1352dafc046 (git://github.com/kazuho/picohttpparser.git)

--- picohttpparser/picohttpparser.h
+++ picohttpparser/picohttpparser.h
@@ -1,6 +1,10 @@
 #ifndef picohttpparser_h
 #define picohttpparser_h
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* contains name and value of a header (name == NULL if is a continuing line
  * of a multiline header */
 struct phr_header {
@@ -17,5 +21,15 @@ int phr_parse_request(const char* buf, size_t len, const char** method,
 		      size_t* path_len, int* minor_version,
 		      struct phr_header* headers, size_t* num_headers,
 		      size_t last_len);
+
+/* ditto */
+int phr_parse_response(const char* _buf, size_t len, int *minor_version,
+              int *status, const char **msg, size_t *msg_len,
+              struct phr_header* headers, size_t* num_headers,
+              size_t last_len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
